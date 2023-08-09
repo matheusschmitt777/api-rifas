ae962ea01a5242f40716a61ffba6035e99f31a76
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-08-08 23:59:27 -0300

    Test Profile

--- src/main/java/com/api/rifas/servies/OrderService.java
+++ src/main/java/com/api/rifas/servies/OrderService.java
@@ -28,7 +28,7 @@ public class OrderService {
 
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public Order createOrder(OrderDTO orderDTO) {
